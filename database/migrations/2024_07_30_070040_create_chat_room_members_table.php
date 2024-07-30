82ecadcf0f5c599c48aff78cafff5253e7cd5094
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-30 14:05:29 +0700

    Commit

---
+++ database/migrations/2024_07_30_070040_create_chat_room_members_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chat_room_members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chat_room_id')->constrained('chat_rooms')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->unique(['chat_room_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_room_members');
+    }
+};
